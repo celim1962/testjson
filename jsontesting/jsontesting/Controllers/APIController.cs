f121ca3c50e0be28b24a3235adee9f7474f07b85
celim1962 <43246276+celim1962@users.noreply.github.com>
2020-11-30 14:08:31 +0800

    addmainfile

---
+++ jsontesting/jsontesting/Controllers/APIController.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+namespace jsontesting.Controllers
+{
+   
+    public class APIController : ApiController
+    {
+        final_pEntities p_db = new final_pEntities();
+        // GET: api/API
+        [EnableCors("*", "*", "*")]
+
+        public JObject Get()
+        {
+            var ans = new
+            {
+                product = from f in p_db.pProductdb
+                              select new
+                              {
+                                  fname = f.Product_name,
+                                  fprice = f.Product_Price
+                              }
+                
+        };
+            string strjson = JsonConvert.SerializeObject(ans, Formatting.Indented);
+            JObject o = JObject.Parse(strjson);
+            return o;
+        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/API/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/API
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/API/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/API/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ jsontesting/jsontesting/Controllers/DefaultController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace jsontesting.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        final_pEntities p_db = new final_pEntities();
+        // GET: api/Default
+        //public IEnumerable<string> Get()
+        public JObject Get()
+        {
+            var ans = new
+            {
+                fname = from f in p_db.pProductdb
+                        select f.Product_name
+            };
+
+            string strjson = JsonConvert.SerializeObject(ans, Formatting.Indented);
+            JObject o = JObject.Parse(strjson);
+            return o;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Default/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
